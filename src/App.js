d4daae6b612a355295d28a528d0e54251c2121e8
Samuel Kirori <samuelkirori300@gmail.com>
2024-05-05 12:12:01 +0300

    Complete challenge

--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
-import logo from './logo.svg';
+
+
 import './App.css';
+import React,{ useEffect,useState } from 'react';  
+import BotForm from './BotForm';
+import Bot from './Bot';
 
 function App() {
+ const [selectedCategory, setSelectedCategory] = useState("All");
+const [bots,setBots]= useState([]);
+const [armyCart, setArmyCart] = useState([]);
+
+  useEffect(() =>{
+    fetch('http://localhost:3000/bots')
+    .then(response => response.json())
+    .then((data) =>{
+      setBots(data);
+    })
+    .catch((error)=>{
+      console.error('Error fetching data:',error);
+    })
+  },[]);
+
+  function handleDeleteBot(deletedBot) {
+   // console.log("In ArmyCart:", deletedBot);
+   const updatedBots = bots.filter((bot) => bot.id !== deletedBot.id);
+  setBots(updatedBots);
+  }
+
+  function handleUpdateBot(updatedBot) {
+   // console.log("In ArmyCart:", updatedBot);
+   const updatedBots = bots.map((bot) => {
+    if (bot.id === updatedBot.id) {
+      return updatedBot;
+    } else {
+      return bot;
+    }
+  });
+  setBots(updatedBots);
+  }
+
+  function handleAddBot(newBot) {
+    setBots([...bots, newBot])  ;
+  }
+
+  function handleCategoryChange(category) {
+    setSelectedCategory(category);
+  }
+
+  const botsToDisplay = bots.filter((bot) => {
+    if (selectedCategory === "All") return true;
+
+    return bot.category === selectedCategory;
+  });
+  
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="ArmyList">
+
+
+    {/* add the onAddItem prop! */}
+    <BotForm onAddBot={handleAddBot} />
+   
+    <h1>MAWOWO GAMERS</h1>
+    <ul className="Bots">
+      {botsToDisplay.map((bot) => (
+        <Bot key={bot.id} 
+        bot={bot} 
+        armyCart={armyCart}
+            setArmyCart={setArmyCart}
+        onUpdateBot={handleUpdateBot} 
+        onDeleteBot={handleDeleteBot}/>
+      ))}
+    </ul>
+    
+  </div>
   );
 }
 
